270b46035f0950052c32c993a4e356a7846b6ab8
Kastuell <sholotun@mail.ru>
2025-08-05 17:10:04 +1000

    change slider

--- index.js
+++ index.js
@@ -1,149 +1,224 @@
-const vasuk_carousel = document.getElementById("vasuk_carousel")
-const vasuk_slides = document.querySelectorAll('.vasuk-slide');
-const vasukArrowLeft = document.getElementById("vasuk-arrow-left")
-const vasukArrowRight = document.getElementById("vasuk-arrow-right")
-const vasukDotContainer = document.getElementById("vasuk-dot-container")
+const vasuk_carousel = document.getElementById("vasuk_carousel");
+const vasuk_slides = document.querySelectorAll(".vasuk-slide");
+const vasukArrowLeft = document.getElementById("vasuk-arrow-left");
+const vasukArrowRight = document.getElementById("vasuk-arrow-right");
+const vasukDotContainer = document.getElementById("vasuk-dot-container");
 
-function createCarousel(elem, slides, slidesToShow, leftArrow, rightArrow, dotContainer) {
+function createCarousel(
+  elem,
+  slides,
+  slidesToShow,
+  leftArrow,
+  rightArrow,
+  dotContainer
+) {
+  if (window.innerWidth >= 300) {
+    slidesToShow = 1;
+  }
+  if (window.innerWidth >= 768) {
+    slidesToShow = 2;
+  }
 
-    let current = 0;
-    let length = slides.length;
-    let slideWidth = 100 / slidesToShow;
-    let dotsLengths = Math.ceil(length / slidesToShow) + 1
+  if (window.innerWidth >= 1024) {
+    slidesToShow = 3;
+  }
 
-    let dotsArr = [];
+  let current = 0;
+  let length = slides.length;
+  let slideWidth = 100 / slidesToShow;
+  let dotsLengths = length - slidesToShow + 1;
+  let dotsArr = [];
 
-    if (dotContainer) {
-        dotContainer.innerHTML = "";
-        // console.log(dotContainer)
-        for (let i = 0; i < dotsLengths; i++) {
-            if (i == 0) {
-                let div = document.createElement('i');
-                div.addEventListener("click", () => goToSlide(i))
-                div.className = "vasuk-dot active-dot";
-                dotsArr.push(div)
-                continue;
-            }
-            let div = document.createElement('i');
-            div.addEventListener("click", () => goToSlide(i))
-            div.className = "vasuk-dot";
-            dotsArr.push(div)
-        }
+  //   zeroing on resize
+  leftArrow.disabled = true;
+  leftArrow.className += " disabled-arrow";
+  rightArrow.disabled = false;
+  rightArrow.className = rightArrow.className.replace(" disabled-arrow", "");
+  elem.style.transform = `translateX(0%)`;
 
-        dotsArr.forEach(item =>
-            dotContainer.append(item)
-        )
+  if (dotContainer) {
+    dotContainer.innerHTML = "";
+    for (let i = 0; i < dotsLengths; i++) {
+      if (i == 0) {
+        let div = document.createElement("i");
+        div.addEventListener("click", () => goToSlide(i));
+        div.className = "dot active-dot";
+        dotsArr.push(div);
+        continue;
+      }
+      let div = document.createElement("i");
+      div.addEventListener("click", () => goToSlide(i));
+      div.className = "dot";
+      dotsArr.push(div);
     }
 
+    dotsArr.forEach((item) => dotContainer.append(item));
+  }
 
+  slides.forEach((slide) => {
+    slide.style.minWidth = `${slideWidth}%`;
+  });
 
-    slides.forEach(slide => {
-        slide.style.minWidth = `${slideWidth}%`;
-    });
+  let dots = document.querySelectorAll(".dot");
 
-
-
-
-    let next = () => {
-        if (current < dotsLengths - 1) {
-            leftArrow.className = leftArrow.className.replace(" disabled-arrow", "")
-            leftArrow.setAttribute("disabled", false);
-            if (dotContainer) dotsArr[current].className = dotsArr[current].className.replace(" active-dot", "");
-            elem.style.transform = `translateX(${(current + 1) * -slideWidth}%)`;
-            current++;
-            if (dotContainer) dotsArr[current].className = `${dotsArr[current]?.className} active-dot`
+  function next() {
+    if (!rightArrow.disabled) {
+      if (current < dotsLengths - 1 || (!dotContainer && current < length)) {
+        leftArrow.className = leftArrow.className.replace(
+          " disabled-arrow",
+          ""
+        );
+        leftArrow.disabled = false;
+        if (dotContainer) {
+          dots[current].className = dots[current].className.replace(
+            " active-dot",
+            ""
+          );
         }
 
-        if (current + 1 == dotsLengths) {
-            rightArrow.className = `${rightArrow.className} disabled-arrow`
-            leftArrow.setAttribute("disabled", true);
-        }
+        elem.style.transform = `translateX(${(current + 1) * -slideWidth}%)`;
+        current++;
+        if (dotContainer) dots[current].className += " active-dot";
+      }
+      if (
+        (dotContainer && current + 1 == dotsLengths) ||
+        (!dotContainer && current + slidesToShow == length)
+      ) {
+        rightArrow.className += " disabled-arrow";
+        rightArrow.disabled = true;
+      }
     }
+  }
 
-    function prev() {
-        if (current <= dotsLengths && current > 0) {
-            rightArrow.className = rightArrow.className.replace(" disabled-arrow", "")
-            rightArrow.setAttribute("disabled", false);
+  function prev() {
+    if (!leftArrow.disabled) {
+      if (current <= dotsLengths && current > 0) {
+        rightArrow.className = rightArrow.className.replace(
+          " disabled-arrow",
+          ""
+        );
+        rightArrow.disabled = false;
 
-            if (dotContainer) dotsArr[current].className = dotsArr[current].className.replace(" active-dot", "")
-            current--;
-            elem.style.transform = `translateX(${current * -slideWidth}%)`
-            if (dotContainer) dotsArr[current].className = `${dotsArr[current].className} active-dot`
-        }
+        if (dotContainer)
+          dots[current].className = dots[current].className.replace(
+            " active-dot",
+            ""
+          );
+        current--;
+        elem.style.transform = `translateX(${current * -slideWidth}%)`;
+        if (dotContainer) dots[current].className += " active-dot";
+      }
 
-        if (current == 0) {
-            leftArrow.className = `${leftArrow.className} disabled-arrow`
-            leftArrow.setAttribute("disabled", true);
-        }
+      if (current == 0) {
+        leftArrow.className += " disabled-arrow";
+        leftArrow.disabled = true;
+      }
     }
+  }
 
-    function goToSlide(index) {
-        if (index > current) {
-            let temp = index - current
-            for (let i = 0; i < temp; i++) next()
-        } else if (index < current) {
-            let temp = current - index
-            for (let i = 0; i < temp; i++) prev()
-        }
+  function goToSlide(index) {
+    if (index > current) {
+      let temp = index - current;
+      for (let i = 0; i < temp; i++) next();
+    } else if (index < current) {
+      let temp = current - index;
+      for (let i = 0; i < temp; i++) prev();
     }
+  }
 
-    function getCurrent() { return current; }
+  function getCurrent() {
+    return current + slidesToShow;
+  }
 
-    return {
-        prev, next, getCurrent, goToSlide
-    }
+  function setCurrentZero() {
+    current = 0;
+  }
+
+  return {
+    prev,
+    next,
+    getCurrent,
+    goToSlide,
+    setCurrentZero,
+  };
 }
 
-function initCarousel(elem, slides, slidesToShow = 1, leftArrow, rightArrow, dotContainer) {
-    if (window.innerWidth >= 768) {
-        slidesToShow = 2;
-    }
+let carousel1 = createCarousel(
+  vasuk_carousel,
+  vasuk_slides,
+  1,
+  vasukArrowLeft,
+  vasukArrowRight,
+  vasukDotContainer
+);
 
-    if (window.innerWidth >= 1024) {
-        slidesToShow = 3;
-    }
+let { next: vNext, prev: vPrev } = carousel1;
+vasukArrowLeft.addEventListener("click", vPrev);
+vasukArrowRight.addEventListener("click", vNext);
 
-    return createCarousel(
-        elem, slides, slidesToShow, leftArrow, rightArrow, dotContainer
-    );
+window.addEventListener("resize", () => {
+  carousel1.setCurrentZero();
+  carousel1 = createCarousel(
+    vasuk_carousel,
+    vasuk_slides,
+    1,
+    vasukArrowLeft,
+    vasukArrowRight,
+    vasukDotContainer
+  );
+});
+
+const membersSlider = document.getElementById("members-slider");
+const membSlides = document.querySelectorAll(".slide-members");
+const membersArrowLeft = document.getElementById("members-arrow-left");
+const membersArrowRight = document.getElementById("members-arrow-right");
+const membersStatus = document.getElementById("members-status");
+
+let carousel2 = createCarousel(
+  membersSlider,
+  membSlides,
+  1,
+  membersArrowLeft,
+  membersArrowRight,
+  null
+);
+
+let { next: mNext, prev: mPrev, getCurrent: mGetCurrent } = carousel2;
+membersStatus.innerHTML = `
+        <span class="slider-status__current">${mGetCurrent()}</span> / ${
+    membSlides.length
+  }
+    `;
+
+function membersNext() {
+  mNext();
+  membersStatus.innerHTML = `
+        <span class="slider-status__current">${mGetCurrent()}</span> / ${
+    membSlides.length
+  }
+    `;
 }
 
+function membersPrev() {
+  mPrev();
+  membersStatus.innerHTML = `
+        <span class="slider-status__current">${mGetCurrent()}</span> / ${
+    membSlides.length
+  }
+    `;
+}
 
-let carousel1 = initCarousel(vasuk_carousel, vasuk_slides, 1, vasukArrowLeft, vasukArrowRight, vasukDotContainer)
+membersArrowLeft.addEventListener("click", membersPrev);
+membersArrowRight.addEventListener("click", membersNext);
 
-let { next: vNext, prev: vPrev, getCurrent: vGetCurrent, goToSlide: vGoToSlide } = carousel1
-
-vasukArrowLeft.addEventListener('click', vPrev)
-vasukArrowRight.addEventListener('click', vNext)
-
-
-// const membersSlider = document.getElementById("members-slider")
-// const membSlides = document.querySelectorAll(".slide-members")
-// const membersArrowLeft = document.getElementById("members-arrow-left")
-// const membersArrowRight = document.getElementById("members-arrow-right")
-// const membersStatus = document.getElementById("members-status")
-
-
-// const { next: mNext, prev: mPrev, getCurrent: mGetCurrent } = createCarousel(membersSlider, membSlides, 1, membersArrowLeft, membersArrowRight, null)
-
-// function membersNext() {
-//     mNext()
-//     membersStatus.innerHTML = `
-//         <span class="slider-status__current">${mGetCurrent() + 1}</span> / ${membSlides.length}
-//     `
-// }
-
-// function membersPrev() {
-//     mPrev()
-//     membersStatus.innerHTML = `
-//         <span class="slider-status__current">${mGetCurrent() + 1}</span> / ${membSlides.length}
-//     `
-// }
-
-// membersArrowLeft.addEventListener("click", membersPrev)
-// membersArrowRight.addEventListener("click", membersNext)
-
-
-window.addEventListener('resize', () => {
-    carousel1 = initCarousel(vasuk_carousel, vasuk_slides, 1, vasukArrowLeft, vasukArrowRight, vasukDotContainer)
+window.addEventListener("resize", () => {
+  carousel2.setCurrentZero();
+  carousel2 = createCarousel(
+    membersSlider,
+    membSlides,
+    1,
+    membersArrowLeft,
+    membersArrowRight,
+    null
+  );
 });
